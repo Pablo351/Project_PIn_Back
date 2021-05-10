66b7070b9b8abc6d76fba80343dfa44da33b9c80
Pablo351 <paex94@gmail.com>
2021-05-10 17:39:23 -0300

    no message

--- config/database.php
+++ config/database.php
@@ -2,36 +2,36 @@
 
 use Illuminate\Support\Str;
 
-$DATABASE_URL = parse_url(getenv("DATABASE_URL"));
+// $DATABASE_URL = parse_url(getenv("DATABASE_URL"));
 
-return [
+// return [
 
-    // …
+//     // …
 
-    'connections' => [
+//     'connections' => [
 
-        // …
+//         // …
 
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => $DATABASE_URL["host"],
-            'port' => $DATABASE_URL["port"],
-            'database' => ltrim($DATABASE_URL["path"], "/"),
-            'username' => $DATABASE_URL["user"],
-            'password' => $DATABASE_URL["pass"],
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-            'sslmode' => 'require',
-        ],
+//         'pgsql' => [
+//             'driver' => 'pgsql',
+//             'host' => $DATABASE_URL["host"],
+//             'port' => $DATABASE_URL["port"],
+//             'database' => ltrim($DATABASE_URL["path"], "/"),
+//             'username' => $DATABASE_URL["user"],
+//             'password' => $DATABASE_URL["pass"],
+//             'charset' => 'utf8',
+//             'prefix' => '',
+//             'schema' => 'public',
+//             'sslmode' => 'require',
+//         ],
 
-        // …
+//         // …
 
-    ],
+//     ],
 
-    // …
+//     // …
 
-];
+// ];
 
 return [
 
